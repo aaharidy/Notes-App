10ce6ef0e5d4d32b58049ddfc1078cb16d8122a1
aaaharidy <a.abdelhamed@leap13.com>
2021-07-07 01:30:52 +0200

    list note

--- notes.js
+++ notes.js
@@ -1,15 +1,11 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
-    return 'Your notes...'
-}
+const getNotes = () => 'Your notes...'
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -23,11 +19,9 @@ const addNote = function (title, body) {
     }
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title;
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length !== notesToKeep.length) {
         saveNotes(notesToKeep);
@@ -37,12 +31,19 @@ const removeNote = function (title) {
     }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.white.bgGreen('Your notes!'));
+
+    notes.forEach(note => console.log(chalk.white.bgBlue(note.title)));
+}
+
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -55,5 +56,6 @@ const loadNotes = function () {
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
